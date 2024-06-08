aa10329cd6538f448d2ddf8f45a3687311038acb
Aamir Siddiqui <aamir_siddiqui17@yahoo.com>
2024-06-09 00:58:32 +0100

    chore: Remove EKS cluster deployment and deletion scripts

--- aws/delete.sh
+++ aws/delete.sh
@@ -5,8 +5,4 @@ echo "Deleting resources..."
 # Delete Redis Cluster
 ./delete-redis.sh
 
-echo "Deleting EKS Cluster..."
-# Delete EKS Cluster
-./deleteEKS.sh
-
 echo "Deletion complete."
--- aws/deploy.sh
+++ aws/deploy.sh
@@ -4,9 +4,5 @@ aws configure set region eu-west-2
 # deploy redis cache
 ./deploy-redis.sh
 
-echo "Deploying EKS Cluster..."
-
-# deploy EKS cluster
-./deployEKS.sh
 
 echo "Deployment complete."
