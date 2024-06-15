84bd301877595ac8a921fda7c8c0d4f66a6420e4
Aamir Siddiqui <aamir_siddiqui17@yahoo.com>
2024-06-15 20:48:26 +0100

    chore: Update firebase_helper.py to support loading Firebase credentials from local file or S3 bucket

--- server/lib/firebase_helper.py
+++ server/lib/firebase_helper.py
@@ -2,12 +2,31 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import os
+import logging
+import boto3
 
-# Current file path 
+# Current file path
 file_path = os.path.abspath(__file__)
+filename = "firebase-sdk.json"
+file_loc = os.path.join(os.path.dirname(file_path), f"../{filename}")
 
+log = logging.getLogger(__name__)
 
-cred = credentials.Certificate(os.path.join(os.path.dirname(file_path), "../firebase-sdk.json"))
+# Check if file exists if not
+if os.path.exists(file_loc):
+    cred = credentials.Certificate(file_loc)
+    log.info("Using local %s", filename)
+else:
+    # Check if s3_bucket exists
+    bucket_name = os.getenv("PRIVATE_S3")
+    if not bucket_name:
+        raise FileNotFoundError(f"{filename} not found and PRIVATE_S3 not set")
+    s3_url = f's3://{bucket_name}/{filename}'
+    s3 = boto3.client('s3')
+    s3.download_file(bucket_name, filename, file_loc)
+    cred = credentials.Certificate(file_loc)
+    log.info("Using %s", s3_url)
+
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
