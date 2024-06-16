a3c833fe32146149cfe386e2b85940ad1476870b
Aamir Siddiqui <aamir_siddiqui17@yahoo.com>
2024-06-16 07:52:44 +0100

    chore: Update AWS App Runner service configuration and deployment script

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -11,6 +11,7 @@ REDIS_STACK_NAME="ai-code-heist-redis-stack"
 APP_STACK_NAME="ai-code-heist-app-stack"
 REDIS_TEMPLATE_FILE="deploy-redis.yaml"
 APP_TEMPLATE_FILE="deploy-app.yaml"
+ECR_REPO_URI="${ECR_REPOSITORY_URI}:latest"
 
 # Deploy the Redis CloudFormation stack
 echo "Deploying the Redis stack..."
@@ -34,6 +35,8 @@ REDIS_CLUSTER_INFO=$(aws elasticache describe-cache-clusters --cache-cluster-id 
 REDIS_CLUSTER_ENDPOINT=$(echo $REDIS_CLUSTER_INFO | jq -r '.CacheClusters[0].CacheNodes[0].Endpoint.Address')
 REDIS_CLUSTER_PORT=$(echo $REDIS_CLUSTER_INFO | jq -r '.CacheClusters[0].CacheNodes[0].Endpoint.Port')
 
+echo "Redis cluster endpoint: $REDIS_CLUSTER_ENDPOINT:$REDIS_CLUSTER_PORT"
+
 # Deploy the App Runner CloudFormation stack
 echo "Deploying the App Runner stack..."
 aws cloudformation deploy \
@@ -46,7 +49,7 @@ aws cloudformation deploy \
     SecurityGroupId=$SECURITY_GROUP_ID \
     RedisClusterEndpoint=$REDIS_CLUSTER_ENDPOINT \
     RedisClusterPort=$REDIS_CLUSTER_PORT \
-    ECRRepositoryUri=$ECR_REPOSITORY_URI \
+    ECRRepositoryUri=$ECR_REPO_URI \
     OpenAIApiKey=$OPENAI_API_KEY \
     AdminKey=$ADMIN_KEY \
     SecretKey=$SECRET_KEY \
--- server/build_and_deploy.sh
+++ server/build_and_deploy.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Load ECR_REGISTRY from .env file
+# Load ECR_REECR_REPOSITORY_URI  from .env file
 export $(cat .env | xargs)
 # Variables
 IMAGE_NAME="ai-code-heist"
-ECR_REPOSITORY="${ECR_REGISTRY}"
 PLATFORM="linux/amd64"
 
 # Rebuild the Docker image for the specified platform
 docker buildx build --platform ${PLATFORM} -t ${IMAGE_NAME}:latest --load .
 
 # Tag the Docker image
-docker tag ${IMAGE_NAME}:latest ${ECR_REPOSITORY}:latest
+docker tag ${IMAGE_NAME}:latest ${ECR_REPOSITORY_URI}:latest
 
 # Log in to Amazon ECR
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${ECR_REGISTRY}
+aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin ${ECR_REPOSITORY_URI}
 
 # Push the Docker image to Amazon ECR
-docker push ${ECR_REPOSITORY}:latest
+docker push ${ECR_REPOSITORY_URI}:latest
